4db212a8925006437117efb0dec3afd60b444ed0
Vitalii Skavronskyi <61649805+vskavronsky@users.noreply.github.com>
2020-10-16 21:56:16 +0300

    Delete DefaultAbility.cpp

--- army/ability/DefaultAbility.cpp
+++
@@ -1,9 +0,0 @@
-#include "DefaultAbility.h"
-
-DefaultAbility::DefaultAbility(Unit* owner): Ability(owner) {}
-
-DefaultAbility::~DefaultAbility() {}
-
-void DefaultAbility::attack(Unit* enemy) {
-    Ability::attack(enemy);
-}
